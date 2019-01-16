63276a6739d012f3616a76c83e02ab834cf77185
Naeem <leguy789@gmail.com>
2019-01-16 22:30:20 +0000

    Rudementary Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="github">
+      <img src="./assets/github.png">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.github{
+  margin-left : 40%;
+  margin-top: 0%;
+}
+</style>
